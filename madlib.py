d900902aca01dd14f95cad7066278de55aec5285
prahe184 <prahek2007@gmail.com>
2025-03-16 14:12:24 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,12 @@
+#String concatination
+#youtuber = "Prahalya"
+#print("Subscribe to " +youtuber)
+#print("Subscribe to {}".format(youtuber))
+#print( f"Subscribe to {youtuber}")
+adjective = input("adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+famous_person = input("famous_person: ")
+print(f"Computer programming is so {adjective}! It makes people so excited all the time because \
+      I love to {verb1} Stay hydratedand be {verb2}.Like you're a {famous_person}....")
+
